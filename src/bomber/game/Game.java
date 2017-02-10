a9eea09cd86fb96e62540d94de9924baf6f5c597
Owen <owenharrijenkins@aol.co.uk>
2017-02-10 12:51:13 +0000

    Updated

--- src/bomber/game/Game.java
+++ src/bomber/game/Game.java
@@ -43,11 +43,11 @@ public class Game{
 		
 		this.gameState = new GameState(map, list);
 		this.physics = new PhysicsEngine(gameState);
-		Player ai = new GameAI("   dasda", new Point(128,128),5, 300, gameState,null);
+		Player ai = new GameAI("   dasda", new Point(128,128),5, 300, gameState, null);
 		list.add(ai);
 		try {
 			
-			this.graphics = new Graphics("Bomb Blitz", 600, 600, this, this.gameState, controlScheme, p1);
+			this.graphics = new Graphics("Bomb Blitz", 1200, 600, this, this.gameState, controlScheme, p1);
 			this.graphics.start();
 			ai.begin();
 			
