65f83aced69ecdfca692b6d2c8ef0b9849fab268
Owen <owenharrijenkins@aol.co.uk>
2017-02-17 10:34:54 +0000

    Added powerup block constants

--- src/bomber/game/Block.java
+++ src/bomber/game/Block.java
@@ -2,9 +2,16 @@ package bomber.game;
 
 public enum Block {
 
-	BLANK, SOLID, SOFT, BLAST	//possible block types for each grid square of the map: 
+	BLANK, SOLID, SOFT, BLAST, PLUS_BOMB, MINUS_BOMB, PLUS_RANGE, MINUS_RANGE, PLUS_SPEED, MINUS_SPEED	//possible block types for each grid square of the map: 
 	//BLANK - nothing
 	//SOLID - indestructible block
 	//SOFT - destructible block
 	//BLAST - explosion blast
+	
+	//PLUS_BOMB - +1 bomb powerup
+	//MINUS_BOMB - -1 bomb powerdown
+	//PLUS_RANGE - +1 bomb range powerup
+	//MINUS_RANGE - -1 bomb range powerdown
+	//PLUS_SPEED - increase speed powerup
+	//MINUS_SPEED - decrease speed powerdown
 }
