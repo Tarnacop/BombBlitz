f5f79c7dad801a37be01eb9d259b77d11fc9ff38
tarnacop <alexandru.blinda@yahoo.com>
2017-02-27 23:03:48 +0000

    Adding Text Rendering

--- src/bomber/renderer/Renderer2.java
+++ src/bomber/renderer/Renderer2.java
@@ -4,8 +4,6 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
 
-import java.util.List;
-
 import org.joml.Matrix4f;
 import org.joml.Vector2f;
 
@@ -18,7 +16,7 @@ import bomber.renderer.shaders.ShaderProgram;
 import bomber.renderer.utils.FileHandler;
 import bomber.renderer.utils.Transformation;
 
-public class Renderer {
+public class Renderer2 {
 
 	private ShaderProgram sceneShader;
 	private final Transformation transformation;
@@ -48,7 +46,6 @@ public class Renderer {
 
 		screen.setClearColour(0f, 0f, 0f, 0f);
 	} // END OF init METHOD
-		// Takes a state to render
 
 	public void setupSceneShader() throws Exception {
 
@@ -62,31 +59,40 @@ public class Renderer {
 
 	} // END OF setupSceneShader METHOD
 
+	// Takes a state to render
 	public void render(Screen screen, GameState state) {
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		// Resize the screen if it needs to be resized
 		if (screen.isResized()) {
+
 			screen.setViewport(0, 0, screen.getWidth(), screen.getHeight());
 			screen.setResized(false);
 		}
-		
+
 		renderScene(screen, state);
+
 	} // END OF render METHOD
 
 	public void renderScene(Screen screen, GameState state) {
 
 		// Bind the shader
 		sceneShader.bind();
+
 		// Set the uniform for the projection matrix
 		projectionMatrix = transformation.getOrthographicProjection(0f, screen.getWidth(), screen.getHeight(), 0f, -1f,
 				1f);
 		sceneShader.setUniform("projection", projectionMatrix);
+
 		// Render each entity of the state
 		Block[][] blocks = state.getMap().getGridMap();
+
 		for (int i = 0; i < blocks.length; i++) {
 			for (int j = 0; j < blocks[0].length; j++) {
+
 				if (blocks[i][j] == Block.SOLID) {
+
 					Vector2f blockCoords = new Vector2f(i * 64f, j * 64f);
 					modelMatrix = transformation.getModelMatrix(blockCoords, 0f, 1f);
 					sceneShader.setUniform("model", modelMatrix);
@@ -97,6 +103,7 @@ public class Renderer {
 					modelMatrix = transformation.getModelMatrix(blockCoords, 0f, 1f);
 					sceneShader.setUniform("model", modelMatrix);
 					softMesh.render();
+
 				} else if (blocks[i][j] == Block.BLAST) {
 
 					Vector2f blockCoords = new Vector2f(i * 64f, j * 64f);
@@ -104,32 +111,35 @@ public class Renderer {
 					sceneShader.setUniform("model", modelMatrix);
 					blastMesh.render();
 				}
+
 			}
 		}
-		List<Player> playerList = state.getPlayers();
-		synchronized (playerList) {
-			for (Player player : playerList) {
-				if (!player.isAlive())
-					continue;
-				modelMatrix = transformation
-						.getModelMatrix(new Vector2f((float) player.getPos().x, (float) player.getPos().y), 0f, 1f);
-				sceneShader.setUniform("model", modelMatrix);
-				player.getMesh().render();
-			}
+
+		for (Player player : state.getPlayers()) {
+			if (!player.isAlive())
+				continue;
+			modelMatrix = transformation
+					.getModelMatrix(new Vector2f((float) player.getPos().x, (float) player.getPos().y), 0f, 1f);
+
+			sceneShader.setUniform("model", modelMatrix);
+
+			player.getMesh().render();
 		}
-		List<Bomb> boombList = state.getBombs();
-		synchronized (boombList) {
-			for (Bomb bomb : boombList) {
-				modelMatrix = transformation
-						.getModelMatrix(new Vector2f((float) bomb.getPos().x, (float) bomb.getPos().y), 0f, 1f);
-				sceneShader.setUniform("model", modelMatrix);
-				bombMesh.render();
-			}
+
+		for (Bomb bomb : state.getBombs()) {
+
+			modelMatrix = transformation.getModelMatrix(new Vector2f((float) bomb.getPos().x, (float) bomb.getPos().y),
+					0f, 1f);
+
+			sceneShader.setUniform("model", modelMatrix);
+
+			bombMesh.render();
 		}
+
 		// Unbind the shader
 		sceneShader.unbind();
 
-	}
+	} // END OF renderScene METHOD
 
 	public void dispose() {
 
@@ -137,7 +147,6 @@ public class Renderer {
 		softMesh.dispose();
 		blastMesh.dispose();
 		bombMesh.dispose();
-
 		if (sceneShader != null) {
 
 			sceneShader.dispose();
