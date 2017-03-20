bc5e7b25acf21163286c9b557be30bd4320cc261
Alexandru Rosu <r.alex128@gmail.com>
2017-03-20 11:39:19 +0000

    Add audio tests to the test suite

--- src/test/suite/TestSuite.java
+++ src/test/suite/TestSuite.java
@@ -3,6 +3,7 @@ package test.suite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import test.audio.AudioManagerTest;
 import test.physics.PhysicsEngineTest;
 import test.AI.AITestSuite;
 import test.game.GameTestSuite;
@@ -12,11 +13,12 @@ import test.UI.UITestSuite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	NetworkingTestSuite.class,
-	AITestSuite.class,
-	GameTestSuite.class,
-	PhysicsEngineTest.class,
-	UITestSuite.class
+		NetworkingTestSuite.class,
+		AudioManagerTest.class,
+		AITestSuite.class,
+		GameTestSuite.class,
+		PhysicsEngineTest.class,
+		UITestSuite.class
 })
 
 
